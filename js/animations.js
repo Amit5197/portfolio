0e9ea8b603bc8f74fe14d16d03b7d5d1397c8ea5
Amit Kumar Pandey <bhai.amit7@gmail.com>
2026-07-21 02:35:19 -0700

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,1 +1,54 @@
+/* ======================================
+   SCROLL ANIMATIONS
+====================================== */
+
+
+const observer=new IntersectionObserver(
+
+(entries)=>{
+
+
+entries.forEach(entry=>{
+
+
+if(entry.isIntersecting){
+
+
+entry.target.classList.add(
+"show"
+);
+
+
+}
+
+
+});
+
+
+},
+
+{
+
+threshold:0.2
+
+}
+
+);
+
+
+
+
+
+document
+.querySelectorAll(
+".skill-card,.project-card,.about-container"
+)
+.forEach(element=>{
+
+
+observer.observe(element);
+
+
+});
+
 
