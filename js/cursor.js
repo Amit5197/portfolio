1bb1f300233bcf2807f83159ff18332fe1a584a5
Amit Kumar Pandey <bhai.amit7@gmail.com>
2026-07-21 02:35:00 -0700

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -1,1 +1,57 @@
+/* ======================================
+   CUSTOM DEVOPS CURSOR
+====================================== */
+
+
+const cursor=document.querySelector(".cursor");
+
+
+
+document.addEventListener(
+"mousemove",
+(e)=>{
+
+
+cursor.style.left=e.clientX+"px";
+
+
+cursor.style.top=e.clientY+"px";
+
+
+});
+
+
+
+
+
+document.querySelectorAll("a,button")
+.forEach(element=>{
+
+
+element.addEventListener(
+"mouseenter",
+()=>{
+
+
+cursor.classList.add("big");
+
+
+});
+
+
+
+
+element.addEventListener(
+"mouseleave",
+()=>{
+
+
+cursor.classList.remove("big");
+
+
+});
+
+
+});
+
 
